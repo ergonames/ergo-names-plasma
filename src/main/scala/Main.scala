a2563f31b40d4216d6568e41cd289354b750730e
Zack Balbin <zack@zackbalbin.com>
2022-12-12 14:45:32 -0600

    set up custom classes

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -8,38 +8,29 @@ import scorex.crypto.hash.{Blake2b256, Digest32}
 
 object Main {
 
-  case class ErgonameHash(base16str: String) {
-    def toBytes: Array[Byte] = {
-      val digest: Digest32 = Blake2b256.hash(base16str)
-      val bytes: Array[Byte] = digest.toArray
-      bytes
-    }
+  case class ErgoName(name: String) {
+    def toErgoNameHash: ErgoNameHash = ErgoNameHash(Blake2b256.hash(name.getBytes("UTF-8")))
   }
 
-  object ErgonameHash {
-    implicit val ergonameHashConversion: ByteConversion[ErgonameHash] = new ByteConversion[ErgonameHash] {
-      override def convertToBytes(t: ErgonameHash): Array[Byte] = t.toBytes
+  case class ErgoNameHash(hashedName: Array[Byte])
 
-      override def convertFromBytes(bytes: Array[Byte]): ErgonameHash = ErgonameHash(new String(bytes))
-    }
+  implicit val nameConversion: ByteConversion[ErgoNameHash] = new ByteConversion[ErgoNameHash] {
+    override def convertToBytes(t: ErgoNameHash): Array[Byte] = t.hashedName
+
+    override def convertFromBytes(bytes: Array[Byte]): ErgoNameHash = ErgoNameHash(bytes)
   }
 
   def main(args: Array[String]): Unit = {
-    val tokenMap = new PlasmaMap[ErgonameHash, ErgoId](AvlTreeFlags.AllOperationsAllowed, PlasmaParameters.default)
+    val tokenMap = new PlasmaMap[ErgoNameHash, ErgoId](AvlTreeFlags.AllOperationsAllowed, PlasmaParameters.default)
 
-    val ergoname: String = convertToBase16("balb")
-    val ergonameHash: ErgonameHash = ErgonameHash(ergoname)
+    val ergoname: ErgoNameHash = ErgoName("balb").toErgoNameHash
     val tokenId: ErgoId = ErgoId.create("0cd8c9f416e5b1ca9f986a7f10a84191dfb85941619e49e53c0dc30ebf83324b")
 
-    val ergonameData: Seq[(ErgonameHash, ErgoId)] = Seq(ergonameHash -> tokenId)
+    val ergonameData: Seq[(ErgoNameHash, ErgoId)] = Seq(ergoname -> tokenId)
     val result: ProvenResult[ErgoId] = tokenMap.insert(ergonameData: _*)
     val opResults: Seq[OpResult[ErgoId]] = result.response
     val proof: Proof = result.proof
-    println(tokenMap.lookUp(ergonameHash))
-  }
-
-  def convertToBase16(str: String): String = {
-    str.getBytes.map("%02x".format(_)).mkString
+    println(tokenMap.lookUp(ergoname))
   }
 
 }
