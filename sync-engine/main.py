873c6b6aa4c86dce04a7cf1b018b608ef703ff0b
Zack Balbin <zack@zackbalbin.com>
2022-12-13 13:59:46 -0600

    add explorer calls

--- sync-engine/main.py
+++ sync-engine/main.py
@@ -3,6 +3,7 @@ from os.path import exists
 import os
 import json
 
+
 def setup():
     if not exists('../db/'):
         os.mkdir('../db/')
@@ -11,28 +12,38 @@ def setup():
         open('../db/txs.json', 'w')
         setup()
 
+
 def getConfig():
     filePath = '../plasma/config.json'
     configJson = json.load(open(filePath))
     parameters = configJson['parameters']
     defaultExplorerApiUrl = parameters['defaultExplorerApiUrl']
     initialTxId = parameters['initialTxId']
-    initialBoxId = parameters['initialBoxId']
-    return [defaultExplorerApiUrl, initialTxId, initialBoxId]
+    return [defaultExplorerApiUrl, initialTxId]
     
 
-def sync(defaultExplorerApiUrl, initialTxId, initialBoxId):
-    print(defaultExplorerApiUrl)
-    print(initialTxId)
-    print(initialBoxId)
+def sync(defaultExplorerApiUrl, initialTxId):
+    url = defaultExplorerApiUrl + 'api/v1/transactions/' + initialTxId
+    response = requests.get(url)
+    tx = response.json()
+    txId = tx['id']
+    registryOutput = tx['outputs'][0]
+    additionalRegisters = registryOutput['additionalRegisters']
+    spentTransactionId = registryOutput['spentTransactionId']
+    txData = { "transactionId": txId, "additionalRegisters": additionalRegisters, "boxId": registryOutput['boxId'], "spentTransactionId": spentTransactionId }
+    txs = json.load(open('../db/txs.json'))
+    txs.append(txData)
+    json.dump(txs, open('../db/txs.json', 'w'))
+    if spentTransactionId is not None:
+        sync(defaultExplorerApiUrl, spentTransactionId)
+
 
 def main():
     setup()
     config = getConfig()
     defaultExplorerApiUrl = config[0]
     initialTxId = config[1]
-    initialBoxId = config[2]
-    sync(defaultExplorerApiUrl, initialTxId, initialBoxId)
+    sync(defaultExplorerApiUrl, initialTxId)
 
 if __name__ == '__main__':
     main()
